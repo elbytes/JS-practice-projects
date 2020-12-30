5d6695011e8864673cba2b13f75728cde17e5d89
El <58517842+elbytes@users.noreply.github.com>
2020-12-30 13:32:18 -0500

    checkAnswer

--- simon-game/script.js
+++ simon-game/script.js
@@ -1,23 +1,63 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPatterns = [];
+var started = false;
+var level = 0;
 
-function nextSequence(){
-    return Math.floor(Math.random() *4);
-}
-
-var randomNumber = nextSequence();
-var randomColor = buttonColors[randomNumber];
-gamePattern.push(randomColor);
-const buttonSound = new Audio("sounds/" + randomColor + ".mp3");
-
-$("#"+ randomColor).fadeOut(10).fadeIn(10);
-//buttonSound.play();
+$(document).keypress(function(){
+    if(!started){
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").on("click", function(){
     var userChosenColor = this.id;
-   console.log(userChosenColor) 
-});
+    playSounds(userChosenColor);
+    userClickedPatterns.push(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPatterns.length-1);
+ });
+
+
+function nextSequence(){
+    $("#level-title").text("Level " + level);
+    var randomNumber =  Math.floor(Math.random() *4);
+    var randomColor = buttonColors[randomNumber];
+    gamePattern.push(randomColor);
+    $("#"+ randomColor).fadeOut(100).fadeIn(100);
+    playSounds(randomColor);
+    level++;
+}
+
+function playSounds(name){
+    const buttonSound = new Audio("sounds/" + name + ".mp3");
+    buttonSound.play();
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(()=>{
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel){
+    console.log(gamePattern[currentLevel]);
+    console.log(userClickedPatterns[currentLevel]);
+    if(userClickedPatterns[currentLevel] == gamePattern[currentLevel]){
+        console.log("success");
+        if(userClickedPatterns.length == gamePattern.length){
+            console.log("success round");
+            setTimeout(()=>{
+                nextSequence();
+            }, 1000);
+            userClickedPatterns=[];
+        }
+    } else{console.log("fail");}
+    
+}
+
 
 
 
