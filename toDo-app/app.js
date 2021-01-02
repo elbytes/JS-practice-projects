3828c11f871c9664040c30a0956da7b7c92715de
elalimardani <elnaz.alimardani@gmail.com>
2021-01-02 01:22:21 -0500

    implement search method

--- toDo-app/app.js
+++ toDo-app/app.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
 const addForm = document.querySelector('.add');
 const ul = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
+//add todo function
 const generateTodo = todo =>{
     const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
                         <span>${todo}</span>
@@ -10,6 +12,19 @@ const generateTodo = todo =>{
     ul.innerHTML+=html;
 };
 
+//search function
+const searchToDos = (term) => {
+    Array.from(ul.children)
+    .filter(todo => !todo.textContent.includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(ul.children)
+    .filter(todo => todo.textContent.includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+
+    };
+
+
 addForm.addEventListener('submit', e=>{
     e.preventDefault();
     const todo = addForm.add.value.trim();
@@ -17,12 +32,17 @@ addForm.addEventListener('submit', e=>{
         generateTodo(todo);
         addForm.reset();
     }
-    
 });
 
-
+//delete todo
 ul.addEventListener('click', e =>{
     if(e.target.classList.contains('delete')){
         e.target.parentElement.remove();
     }
+});
+
+//search
+search.addEventListener('keyup', () => {
+    const term = search.value.trim();
+    searchToDos(term);
 });
