47ceca933eb2c6ddac4ce10221962b4491284e83
elalimardani <elnaz.alimardani@gmail.com>
2021-01-02 00:53:22 -0500

    add new toDo

---
+++ toDo-app/app.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+const addForm = document.querySelector('.add');
+const ul = document.querySelector('.todos');
+
+const generateTodo = todo =>{
+    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+                        <span>${todo}</span>
+                        <i class="far fa-trash-alt delete"></i>
+                    </li>`;
+    ul.innerHTML+=html;
+}
+
+addForm.addEventListener('submit', e=>{
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+    generateTodo(todo);
+});
