3d55cd7a0d72e37937cf0fb5c6f1ca309c4e831d
elalimardani <elnaz.alimardani@gmail.com>
2021-01-04 13:28:27 -0500

    get current weather api call

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -14,7 +14,6 @@ const getCity = async(city)=>{
 const getCurrent = async(locationKey)=>{
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${locationKey}?apikey=${key}`;
-
     const response = await fetch(base + query);
     const data = await response.json();
     return data[0];
