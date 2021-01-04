816b1c2c7dc9193d6431bac6b8c76ba1b3858113
elalimardani <elnaz.alimardani@gmail.com>
2021-01-04 13:26:42 -0500

    getCity fetch call

--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,3 +1,30 @@
+//jshint esversion:8
 const key = 'bNRjpYXo7CgUDUvuFAYtT1leiklKikzz';
+//get city 
+const getCity = async(city)=>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
 
-const 
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+//get current weather
+const getCurrent = async(locationKey)=>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
+const city = getCity('statecollege').then(data => {
+    return getCurrent(data.Key);
+}).then(data => {
+    console.log(data);
+})
+.catch(err => console.log(err.message));
+
+getCurrent(335315);
