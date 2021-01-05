5eb92d3596004da731097985d8ca9c75b9571971
elalimardani <elnaz.alimardani@gmail.com>
2021-01-04 19:07:22 -0500

    update city function to get data from getCurrent

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -1,10 +1,23 @@
 //jshint esversion:8
 const cityInput = document.querySelector('form');
+
+const updateCity = async (city) =>{
+    const cityData = await getCity(city); //forecast.js loads before app.js
+    const weather = await getCurrent(cityData.Key); 
+    return {
+        cityData: cityData,
+        weather: weather
+    };
+};
+
 cityInput.addEventListener('submit', e =>{
     e.preventDefault();
     //get city value
     const city = cityInput.city.value.trim();
     cityInput.reset();
 
-    
-});
+    //update the ui with new city
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    });
