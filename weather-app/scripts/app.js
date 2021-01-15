fb005a4868c8f08684b389929f037223b4a901e8
elalimardani <elnaz.alimardani@gmail.com>
2021-01-15 08:14:49 -0500

    html structure

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -4,7 +4,8 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
-
+const forecast = new Forecast();
+console.log(forecast);
 
 const updateUi = (data)=>{
     //using destructuring:
