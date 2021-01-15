22a7290a11603dc9d62b33b881e3af2ee569cc2d
elalimardani <elnaz.alimardani@gmail.com>
2021-01-15 10:37:23 -0500

    add query for specific room

--- live-chatroom/scripts/chat.js
+++ live-chatroom/scripts/chat.js
@@ -1,3 +1,4 @@
+//jshint esversion:8
 //add new chat documents
 class Chatroom{
     constructor(room, username){
@@ -8,17 +9,38 @@ class Chatroom{
 
     async addChat(message){
         const now = new Date();
+        //format a chat object
         const chat ={
             message,
             username: this.username,
             room: this.room,
             created_at : firebase.firestore.Timestamp.fromDate(now)
         };
-
+        //save chat document
         const response = await this.chats.add(chat);
         return response;
     }
+
+    getChats(callback){
+        this.chats
+        .where('room', '==', this.room)
+        .onSnapshot(onSnapshot =>{
+            onSnapshot.docChanges().forEach(change =>{
+                if(change.type === 'added'){
+                    //update the ui
+                    callback(change.doc.data());
+                }
+            });
+        });
+    }
 }
+
+const chatroom = new Chatroom('general', 'el');
+
+    chatroom.getChats((data) =>{
+        console.log(data);
+    });
+
 //setting up a real time listener to get new chats
 
 //updating the username
