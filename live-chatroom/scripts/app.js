6923ece9f6fd41cf317b817ae4618199ccc53b1b
elalimardani <elnaz.alimardani@gmail.com>
2021-01-16 15:06:48 -0500

    add update username notification

--- live-chatroom/scripts/app.js
+++ live-chatroom/scripts/app.js
@@ -2,6 +2,8 @@
 //dom queries
 const chatList = document.querySelector('.chat-list');
 const newChat = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 //add a new chat
 newChat.addEventListener('submit', e=>{
@@ -15,6 +17,16 @@ newChat.addEventListener('submit', e=>{
 //class instances
 const chatroom = new Chatroom('general', 'El');
 const chatUi = new ChatUI(chatList);
+
+//update username
+newNameForm.addEventListener('submit', e=>{
+        e.preventDefault();
+        const newName = newNameForm.name.value.trim();
+        chatroom.updateName(newName);
+         newNameForm.reset();
+         updateMssg.innerText = `Username update to ${newName}`;
+         setTimeout(()=>updateMssg.innerText = '', 3000);
+});
 
 //get the chats and render
 chatroom.getChats(data => chatUi.render(data));
